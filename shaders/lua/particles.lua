59b11a5fa2085fabb065df2b8ab7801f32d461e1
Justas Dabrila <justasdabrila@gmail.com>
2019-06-25 20:41:15 +0000

    more particules

--- shaders/lua/particles.lua
+++ shaders/lua/particles.lua
@@ -1,18 +1,8 @@
---[[
-function render_fb_to_screen()
-    local vert = gl_load_shader_part(r, "fb to screen vert", GL_VERTEX, "vertex/one_quad.vertex_shader")
-    local frag = gl_load_shader_part(r, "fb to screen frag", GL_FRAGMENT, "fragment/7_texture_copy.fragment_shader")
-    local shader = gl_load_shader(r, "fb to screen shader", {vert, frag})
-
-    gl_use_shader(r, shader)
-    gl_set_default_uniforms(r)
-    gl_draw_quad(r)
-end
---]]
+local frag_name = "013_particles_animated"
 
 function render_background() 
     local vert = gl_load_shader_part(r, "alpha checker quad", GL_VERTEX, "vertex/one_quad.vertex_shader")
-    local frag = gl_load_shader_part(r, "alpha checker frag", GL_FRAGMENT, "fragment/6_alpha_checkers.fragment_shader")
+    local frag = gl_load_shader_part(r, "alpha checker frag", GL_FRAGMENT, "fragment/006_alpha_checkers.fragment_shader")
     local shader = gl_load_shader(r, "alpha checkers shader", {vert, frag})
 
     gl_use_shader(r, shader)
@@ -22,7 +12,7 @@ end
 
 function render_shader()
     local vert = gl_load_shader_part(r, "main quad", GL_VERTEX, "vertex/one_quad.vertex_shader")
-    local frag = gl_load_shader_part(r, "main frag", GL_FRAGMENT, "fragment/5_particles.fragment_shader")
+    local frag = gl_load_shader_part(r, "main frag", GL_FRAGMENT, "fragment/" .. frag_name .. ".fragment_shader")
     local shader = gl_load_shader(r, "main shader", {vert, frag})
 
     gl_use_shader(r, shader)
@@ -37,16 +27,7 @@ function render()
     gl_default_viewport(r)
     gl_default_fb(r)
 
-    --[[
-    local fb = gl_create_fb(r, "deferred fb", {
-        color = "rgba"
-    });
-
-    gl_use_fb(r, fb)
-    --]]
-
-
-    render_background()
+    --render_background()
     render_shader()
 
 end
